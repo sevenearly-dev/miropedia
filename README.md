89e104be5f6eb7ffceb9d3ee78ac2082bd76ca1d
sevenearly-dev <ssshi@fillume.com>
2023-05-17 15:37:43 +0800

    fix

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# miropedia
+# Nextjs Tailwind Blog
+
+## tempalte
+
+https://github.com/timlrx/tailwind-nextjs-starter-blog
+
+## techs
+
+- Next.js
+- Tailwind CSS 3.0
+
+```bash
+npm run dev
+```
---
+++ data/projectsData.ts
@@ -0,0 +1,10 @@
+const projectsData = [
+  {
+    title: 'vscode-fanyi',
+    description: `vscode 翻译插件`,
+    imgSrc: '/static/images/vscode.png',
+    href: 'https://marketplace.visualstudio.com/items?itemName=maqi1520.everest-fanyi',
+  },
+]
+
+export default projectsData
