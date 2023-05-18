e8ad4c9cf8f8f4d50890965da6ecd1a495a58e9a
sevenearly-dev <ssshi@fillume.com>
2023-05-18 12:40:55 +0800

    fix

--- src/layouts/AuthorLayout.tsx
+++ src/layouts/AuthorLayout.tsx
@@ -15,7 +15,7 @@ export default function AuthorLayout({ children, frontMatter }: Props) {
 
   return (
     <>
-      <PageSEO title={`关于 - ${name}`} description={`关于我 - ${name}`} />
+      <PageSEO title={`${Locale.About.Title}- ${name}`} description={`${Locale.About.Me} - ${name}`} />
       <div className="divide-y">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
